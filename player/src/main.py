9f1f853f3ad1ca13be724b363178894925055b7d
yac771 <yacinekebdani77100@gmail.com>
2026-06-15 19:19:35 +0200

    Add files via upload

--- player/src/main.py
+++ player/src/main.py
@@ -1,6 +1,9 @@
 import sys, os, logging
-# CORRECTION IMPORTANTE: On n'importe PAS QApplication directement ici, car s'il y a un 
-# double import avec launcher.py, ca fait crasher le systeme C++ sous-jacent.
+
+# CORRECTION EXPERT DU CRASH QWEBENGINEWIDGETS
+# PyQtWebEngine DOIT etre importe AVANT la creation de toute instance QApplication.
+# C'est une regle absolue de C++ / Qt qui fait crasher PyInstaller si elle n'est pas respectee.
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5 import QtWidgets, QtCore
 
 def main():
@@ -22,18 +25,17 @@ def main():
     config_path = os.path.join(OMNI_DIR, 'config', 'schedule.json')
     
     # Nettoyage absolu des arguments
-    # Si Windows envoie des chemins bizarres a l'exe, on les nettoie.
     safe_argv = [sys.argv[0], "--no-sandbox", "--disable-gpu-compositing"]
     
-    # On s'assure qu'une seule application existe
     app = QtWidgets.QApplication.instance()
     if not app:
+        # On force les flags globaux pour que WebEngine soit content
+        QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
         app = QtWidgets.QApplication(safe_argv)
         
     if hasattr(QtCore.Qt, 'BlankCursor'):
         app.setOverrideCursor(QtCore.Qt.BlankCursor)
         
-    # Import retarde de l'UI
     from player.src.player import SignagePlayer
     player = SignagePlayer(config_path)
     player.show()
